5b04c16ea32d9603eae26117b100451d76ef1972
parit9222 <140873607+parit9222@users.noreply.github.com>
2024-05-03 18:46:58 +0530

    Create api routes and use database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,6 +14,21 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Server is runing on  port 3000!");
-})
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
