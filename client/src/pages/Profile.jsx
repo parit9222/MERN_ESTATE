78e951a417ebf0287bfb3aede44962283ac3e865
parit9222 <140873607+parit9222@users.noreply.github.com>
2024-05-05 21:31:20 +0530

    Add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -49,10 +49,10 @@ export default function Profile() {
   };
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value});
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
@@ -75,6 +75,23 @@ export default function Profile() {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -84,25 +101,25 @@ export default function Profile() {
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
         <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
         <p className='text-sm self-center'>
-        {fileUploadError ? 
-          (
-            <span className='text-red-700'>Error Image upload(image must be less than 2MB)</span>
-          )
-          :
-          filePerc > 0 && filePerc < 100 ?
-          (
-            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
-          ) 
-          :
-          filePerc === 100 ? 
-          (
-            <span className='text-green-700'>Image successfully uploaded!</span>
-          )
-          :
-          (
-            ''
-          )  
-        }
+          {fileUploadError ?
+            (
+              <span className='text-red-700'>Error Image upload(image must be less than 2MB)</span>
+            )
+            :
+            filePerc > 0 && filePerc < 100 ?
+              (
+                <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+              )
+              :
+              filePerc === 100 ?
+                (
+                  <span className='text-green-700'>Image successfully uploaded!</span>
+                )
+                :
+                (
+                  ''
+                )
+          }
         </p>
 
         <input type="text" placeholder='User Name' defaultValue={currentUser.username} id='username' className='border p-3 rounded-lg' onChange={handleChange} />
@@ -114,7 +131,7 @@ export default function Profile() {
       </form>
 
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Up</span>
       </div>
 
